76735bc9990e4b7e176c8057b744d93b63cb7c5d
Victor Lopez <victorandreslopez314@gmail.com>
2025-01-14 20:22:10 -0600

    Finish parser arguments and add task_manager class
    Added the delete, update, mark-in-progress and mark-complete parser
    arguments for the task cli command in the task-cli file, and created
    the task_manager class in the manager.py file. Still need to link the
    file to task-cli file, but am trying to finish up the functionality before
    linking.

--- manager.py
+++ manager.py
@@ -1,2 +1,17 @@
-import datetime, json, shutil, pathlib
+from datetime import datetime
+import json, shutil, pathlib
+
+class task_manager:
+    """"task_manager class will create tasks and apply commands to the tasks"""
+    def __init__(self,id=None,description=None,status='todo',createdAt=None,updatedAt=None):
+        self.id=id
+        self.description=description
+        self.status=status
+        self.createdAt=createdAt
+        self.updatedAt=updatedAt
+    def add_task(self, descript):
+        task = task_manager()
+        task.id=len(tasks) # add an array containing tasks later which will grab how many tasks we have from the json file
+        task.description = descript
+        task.createdAt=datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
